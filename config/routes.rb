8187c02e2535e42574e41e0a8fa1f2da3d8cb1cb
kiennguyenxuan <kien.framgia@gmail.com>
2017-09-12 19:19:12 +0700

    Chapter 03

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'staticpages/home'
+  get 'staticpages/help'
+  get 'staticpages/about'
+root 'staticpages#home'
 end
